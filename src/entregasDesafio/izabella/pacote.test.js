c1d37c9cbe96db065b08f6968bca6143309c9e5d
izabella-m <izabella.pessoa03@outlook.com>
2024-06-08 17:24:08 -0300

    Meus testes

--- src/entregasDesafio/izabella/pacote.test.js
+++ src/entregasDesafio/izabella/pacote.test.js
@@ -2,19 +2,35 @@ const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote')
 
 describe('Calcular o pacote correto para cada projeto', () => {
     test('Retorne um pacote básico com o número total de horas sejam até 50h', () => {
-    const totalDeHorasPorProjeto = 49
+        const totalDeHorasPorProjeto = 49
 
-    const resultado = calcularPacote(totalDeHorasPorProjeto)
+        const resultado = calcularPacote(totalDeHorasPorProjeto)
 
-    expect(resultado).toEqual('pacote_basico')
+        expect(resultado).toEqual('pacote_basico')
     })
 
     test('Retorna o pacote básico caso o número total de horas seja exatamente 58h', () => {
-    const totalDeHorasPorProjeto = 100
+        const totalDeHorasPorProjeto = 100
 
-    const resultado = calcularPacote(totalDeHorasPorProjeto)
+        const resultado = calcularPacote(totalDeHorasPorProjeto)
 
-    expect(resultado).toEqual('pacote_intermediario')
+        expect(resultado).toEqual('pacote_intermediario')
     })
+
+    test('Retorna o pacote intermediário caso o número total de horas seja exatamente 51h', () => {
+        const totalDeHorasPorProjeto = 51
+
+        const resultado = calcularPacote(totalDeHorasPorProjeto)
+
+        expect(resultado).toEqual('pacote_premium')
+    });
+
+    test('Retorna o pacote premium caso o número total de horas seja até 199h', () => {
+        const totalDeHorasPorProjeto = 199
+
+        const resultado = calcularPacote(totalDeHorasPorProjeto);
+
+        expect(resultado).toEqual('pacote_premium')
+    });
 }
 )
--- src/entregasDesafio/izabella/valorProjeto.test.js
+++ src/entregasDesafio/izabella/valorProjeto.test.js
@@ -24,4 +24,37 @@ describe('Valor total do projeto', () => {
         //verificação
         expect(resultado).toEqual(4312)
     })
+
+    test('Retornar valor total de um projeto personalizado com pacte premium e valor hora ', () => {
+        const funcionalidades = [
+            'setup',
+            'formulario',
+            'responsividade',
+            'construcao_1_pagina',
+            'ssr',
+            'integracao_mailchimp',
+            'otimizacao_seo'
+        ]
+        //acao
+        const valorHora = 120
+
+        pacote.calcularPacote.mockReturnValue('pacote_premium')
+        const resultado = calcularValorTotalProjeto(funcionalidades, valorHora)
+        //verificacao
+        expect(resultado).toBeGreaterThan(12000)
+    });
+
+
+    test('Retorna o valor total de um projeto com numero total ultrapassando a quantidade de horas máxima por pacote', () => {
+        //acao
+        const totalDeHorasPorProjeto = 220
+        const valorHora = 150
+
+        pacote.calcularPacote.mockReturnValue('pacote_premium')
+
+        const resultado = calcularValorTotalProjeto(totalDeHorasPorProjeto, valorHora)
+        //verificacao
+        expect(resultado).toEqual(33000)
+    });
+
 })
