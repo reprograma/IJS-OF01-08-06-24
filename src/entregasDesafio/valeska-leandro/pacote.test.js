091c9404424f674e437ae2c00bcb0ae7df00a497
Valeska Leandro <valeskaleandro@gmail.com>
2024-06-08 16:42:25 -0300

    :test_tube: test: Adicona testes para pacotes intermediário e premium.

--- src/entregasDesafio/valeska-leandro/pacote.test.js
+++ src/entregasDesafio/valeska-leandro/pacote.test.js
@@ -22,4 +22,48 @@ describe('Calcular o pacote correto para cada projeto', () => {
     //verification
     expect(resultado).toBe('pacote_basico')
   })
+
+  test('Retorna um pacote intermediário caso o número total de horas seja superior a 51h e inferior a 100h.', () => {
+    //setup 
+    const totalDeHoras = 88
+
+    //action
+    const resultado = calcularPacote(totalDeHoras)
+
+    //verification
+    expect(resultado).toBe('pacote_intermediario')
+  })
+
+  test('Retorna o pacote itermediário caso o número total de horas seja exatamente 100h.', () => {
+    //setup 
+    const totalDeHoras = 100
+
+    //action
+    const resultado = calcularPacote(totalDeHoras)
+
+    //verification
+    expect(resultado).toBe('pacote_intermediario')
+  })
+
+  test('Retorna um pacote premium caso o número total de horas seja superior a 101h e inferior a 200h.', () => {
+    //setup 
+    const totalDeHoras = 165
+
+    //action
+    const resultado = calcularPacote(totalDeHoras)
+
+    //verification
+    expect(resultado).toBe('pacote_premium')
+  })
+
+  test('Retorna o pacote premium caso o número total de horas seja exatamente 200h.', () => {
+    //setup 
+    const totalDeHoras = 200
+
+    //action
+    const resultado = calcularPacote(totalDeHoras)
+
+    //verification
+    expect(resultado).toBe('pacote_premium')
+  })
 })
